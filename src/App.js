a2dcf604f186046fcb2de4aeea53bc1941e9ee20
samuelALSN <samuelgwfirst@gmail.com>
2020-02-29 14:22:14 +0000

    feat: consuming openWeather Map API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react'
-
 import Card from './components/Card'
-import data from './data.json'
+import axios from 'axios'
 import './App.css'
-
+// SET OF URL CONSTANTS 
+const APPID = '92a66c3676ed75bda5d59d210a5a8c75'
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherData: null
+    }
+    this.onSortedData = this.onSortedData.bind(this)
+
+  }
+
+  onSortedData(result) {
+    // const sortedData =[];
+    // for (let i = 0; i < result.data.list.length; i+3) {
+    //   // console.log(result.data.list[i])
+    //   sortedData.push(result.data.list[i])
+    // }
+    this.setState({
+      weatherData: result.data.list
+    })
+  }
+
+  componentDidMount() {
+    axios(`http://api.openweathermap.org/data/2.5/forecast?q=Lomé,togo&units=metric&type=accurate&APPID=${APPID}`)
+      .then(result => this.onSortedData(result))
+      .catch(error => error)
+  }
+
+
   render() {
-    console.log(data)
+    const { weatherData } = this.state
     return (
       <div className='App'>
         <CardWrapper>
           <Card
-            list={data}
+            list={weatherData}
           />
         </CardWrapper>
       </div>
@@ -24,7 +51,7 @@ class App extends Component {
 const CardWrapper = ({ children }) => {
   return (
     <div className="container">
-        {children}
+      {children}
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,27 +3,34 @@ import { Link } from 'react-router-dom'
 
 class Card extends Component {
 
+    getDay = date => {
+        let weekday = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday']
+        return weekday[new Date(date).getDay()]
+
+    }
     render() {
         const { list } = this.props
+        console.log(list)
         return (
             <div className="card-wrapper">
                 {Array.isArray(list) ?
                     list.map(item =>
-                        <section key={item.objectID} className="card anim-flip">
+                     new Date(item.dt_txt).getHours() === 15 &&
+
+                        <section key={item.dt} className="card anim-flip">
                             <header>
-                                <h1 className="card-header">{item.day}</h1>
+                                <h1 className="card-header">{this.getDay(item.dt_txt)}</h1>
                             </header>
-                            <p className="card-temp box-highlight">{item.highTemperature}</p>
-                            <p className="card-temp box-highlight">{item.lowTemperature}</p>
+                            <p className="card-temp box-highlight">{item.main.temp_max}</p>
+                            <p className="card-temp box-highlight">{item.main.temp_min}</p>
                             <div className="icon">
                                 <div className="cloud-group">
-                                    <span className="icon-cloud cloud-circle shadow-cloud-clip"></span>
-                                    <span className="icon-cloud cloud-base shadow-cloud-clip"></span>
+                                    <img src={require(`../images/${item.weather[0].icon}.svg`)} alt="" />
                                 </div>
                             </div>
-                            <p className="card-info">{item.observation}</p>
+                            <p className="card-info"></p>
                             <span>
-                                <Link to={`/previsions/${item.objectID}`}> Voir </Link>
+                                <Link to={`/previsions/${item.dt}`}> Voir </Link>
                             </span>
                         </section>
 
@@ -31,7 +38,7 @@ class Card extends Component {
                     :
                     <section className="card anim-flip">
                         <header>
-                            <h1 className="card-header">NO DATA </h1>
+                            <h1 className="card-header">Loading... </h1>
                         </header>
                         <p className="card-temp box-highlight"></p>
                         <p className="card-temp box-highlight"></p>
